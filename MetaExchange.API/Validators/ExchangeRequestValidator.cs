e1f4a6492c4f3840152caa1a99a4d61426e7bd95
Oleksandr Slinkin <Slinkin.operative@gmail.com>
2023-08-24 14:34:45 +0300

    Created Controller, request and response models, Added Validator, registered all services

---
+++ MetaExchange.API/Validators/ExchangeRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using MetaExchange.API.Controllers.Requests;
+
+namespace MetaExchange.API.Validators
+{
+    public class ExchangeRequestValidator : AbstractValidator<ExchangeRequest>
+    {
+        public ExchangeRequestValidator()
+        {
+            RuleFor(x => x.Amount)
+                .NotEmpty().WithMessage("Amount should not be 0")
+                .GreaterThan(0).WithMessage("Amount should be greater 0");
+
+            RuleFor(x => x.RequestType)
+                .IsInEnum().WithMessage("Invalid reques type");
+        }
+    }
+}
