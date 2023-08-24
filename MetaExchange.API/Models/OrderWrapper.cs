10f1e2eb9443f4c01340d7c77d31c4f50dce32d3
Oleksandr Slinkin <Slinkin.operative@gmail.com>
2023-08-24 14:04:04 +0300

    Added required models, small cleanup

---
+++ MetaExchange.API/Models/OrderWrapper.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MetaExchange.API.Models
+{
+    public class OrderWrapper
+    {
+        [JsonPropertyName("Order")]
+        public Order Order { get; set; }
+    }
+}
